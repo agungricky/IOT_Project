c40cfa6dad5bc3c7f492396293ffb96a51b0dfe0
agungricky07.github.io <agungricky07@gmail.com>
2025-02-06 13:43:31 +0700

    06/02/2025 Master Original

---
+++ resources/views/main/footer.blade.php
@@ -0,0 +1,14 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto py-2">
+        <div class="copyright text-center my-auto">
+            <span>copyright &copy;
+                <script>
+                    document.write(new Date().getFullYear());
+                </script> - distributed by
+                <b><a href="#" target="_blank">Ruang Robot</a></b>
+            </span>
+        </div>
+    </div>
+</footer>
+<!-- Footer -->
---
+++ resources/views/main/sidebar.blade.php
@@ -0,0 +1,16 @@
+<!-- Sidebar -->
+<ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <div class="sidebar-brand-icon">
+        <img src="img/logo/logo2.png">
+      </div>
+      <div class="sidebar-brand-text mx-3">IOT Project</div>
+    </a>
+    <hr class="sidebar-divider my-0">
+    <li class="nav-item active">
+      <a class="nav-link" href="">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Dashboard</span></a>
+    </li>
+  </ul>
+  <!-- Sidebar -->
